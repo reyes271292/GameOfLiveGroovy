11e050573370689c929a9f91b5ad96356c88cd39
Francisco Reyes <francisco@makingdevs.com>
2016-02-22 12:56:27 -0600

    Se agrega prueba de regla 1

--- src/main/groovy/com/makingdevs/Organism.groovy
+++ src/main/groovy/com/makingdevs/Organism.groovy
@@ -19,19 +19,23 @@ class Organism{
   def find_neighbors(x, y){
     def coordinates_for_neighbors = coordinates(x, y)
     def matrix = cells
-    println "Matriz "+matrix
-    int pos=0
+    //println "Matriz "+matrix
     def neighbors = []
     for(int fila=0;fila<matrix.size;fila++){
       for(int col=0;col<matrix.size;col++){
           if(x!=fila || y!=col){
-            println "Vecinos "+matrix[fila][col]
+            //println "Posicion: "+fila+" "+col
+            //println "Vecinos "+matrix[fila][col]
             neighbors << matrix[fila][col]
           }
       }
     }
     println "---- "+neighbors
     neighbors
+  }
+
+  def might_die_because_has_fewer_than_two_neighbours(living_cell, neighborhood){
+    
   }
     
 }
