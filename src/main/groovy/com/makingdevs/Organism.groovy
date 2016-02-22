89bf29cf29c4ba52817cb6eba555b617007d351b
Francisco Reyes <francisco@makingdevs.com>
2016-02-22 13:18:53 -0600

    Se agrega prueba next state

--- src/main/groovy/com/makingdevs/Organism.groovy
+++ src/main/groovy/com/makingdevs/Organism.groovy
@@ -30,12 +30,40 @@ class Organism{
           }
       }
     }
-    println "---- "+neighbors
     neighbors
   }
 
-  def might_die_because_has_fewer_than_two_neighbours(living_cell, neighborhood){
-    
+  def might_die_because_has_fewer_than_two_neighbours(cell, neighbors){
+    neighbors.count(1) < 2
+  }
+  
+  def might_live_because_has_two_or_three_neighbours(cell, neighbors){
+    (neighbors.count(1) >= 2 && neighbors.count(1) <= 3)
+  }
+
+  def might_die_because_has_more_than_tree_neighbours(cell,neighbors){
+    neighbors.count(1) > 3
+  }
+
+  def might_reborn_because_has_exactly_three_neighbours(cell, neighbors){
+    (neighbors.count(1)==3)
+  }
+
+  def next_state(){
+    matrix = Matrix.rows @cells
+    matrix.each_with_index do |cell, x, y|
+      neighbors = find_neighbors(x, y)
+      case cell
+        when 1
+          @next_state[x][y] = 0 if(might_die_because_has_fewer_than_two_neighbours(cell, neighbors) or might_die_because_has_more_than_tree_neighbours(cell, neighbors))
+          @next_state[x][y] = 1 if(might_live_because_has_two_or_three_neighbours(cell, neighbors))
+        else
+          @next_state[x][y] = might_reborn_because_has_exactly_three_neighbours(cell, neighbors) ? 1 : 0
+      end
+    end
+    @next_state
+    p @next_state
+  end
   }
     
 }
